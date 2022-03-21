f7e3aa000c6641d2e4912c7efbf1c6e27bb0959c
Nico G <nicolas.giusti@nextsourcia.com>
2022-03-21 12:08:05 +0100

    [fix] Adding "levels" param for getMenu

--- src/api/menu/index.js
+++ src/api/menu/index.js
@@ -1,8 +1,8 @@
 const axios = require('../../lib/AxiosInstance');
 
-const getMenu = async (categoryCode, lang = 'fr') => {
+const getMenu = async (categoryCode, lang = 'fr', levels = undefined) => {
     try {
-        const response = await axios.post(`v2/component/ns-menu/${categoryCode}`, { lang });
+        const response = await axios.post(`v2/component/ns-menu/${categoryCode}`, { lang, levels });
         return response.data;
     } catch(err) {
         console.error('menu.getMenu');
