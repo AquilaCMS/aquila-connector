ad24a40a9cc940d33df3d4352399b4cc554330e9
Nico G <nicolas.giusti@nextsourcia.com>
2023-01-27 17:34:50 +0100

    feat: new API territory

---
+++ src/api/territory/index.js
@@ -0,0 +1,22 @@
+const axios = require('../../lib/AxiosInstance');
+const utils = require('../../lib/utils');
+
+// GET territories list
+const getTerritories = async (postBody = {}) => {
+    const _defaultPostBody = {
+        PostBody: {
+            limit   : 9999
+        }
+    };
+    const _postBody = utils.deepMergeObjects(_defaultPostBody, postBody);
+    try {
+        const response = await axios.post('v2/territories', _postBody);
+        return response.data;
+    } catch(err) {
+        throw new utils.ConnectorError(err?.response?.data?.status, err?.response?.data?.message);
+    }
+};
+
+module.exports = {
+    getTerritories
+}
