ea667a751852d7e771df7ff91d0d5d8b478d1d35
Yos666666 <71755422+Yos666666@users.noreply.github.com>
2024-07-10 23:58:47 +0200

    fix: bug return langs

--- src/api/site/index.js
+++ src/api/site/index.js
@@ -5,7 +5,7 @@ const getSiteInfo = async (lang = 'fr') => {
         const themeConfig = await customFetch.post('v2/themeConfig', { lang, PostBody: {} });
         const config = await customFetch.post('v2/config', { PostBody: { structure: { 'environment.siteName': 1, 'environment.displayingReviews': 1 } } });
         const langs = await customFetch.post('v2/languages', { PostBody: { limit: 99 } });
-        return { themeConfig: { ...themeConfig.config }, ...config, langs };
+        return { themeConfig: { ...themeConfig.config }, ...config, langs: langs.datas };
     } catch(err) {
         return { datas: {} };
     }
