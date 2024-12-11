09180089b580c67e98194fbecc445eb8df0e951b
Yos666666 <71755422+Yos666666@users.noreply.github.com>
2024-12-11 18:04:03 +0100

    feat : add get gallery by code method

--- src/api/gallery/index.js
+++ src/api/gallery/index.js
@@ -2,6 +2,23 @@ const customFetch = require('../../lib/FetchInstance');
 const utils = require('../../lib/utils');
 
 /**
+ * Get gallery by code
+ * @async
+ * @param {Object} params - URL params
+ * @param {String} params.code - The code of the gallery
+ * @param {Object} options - Fetch options (default: {})
+ * @returns An object
+ * @throws {ConnectorError} Throws a ConnectorError if the request fails, containing error code, message, and message code.
+ */
+const getGallery = async ({code}, options = {}) => {
+    try {
+        return await customFetch.get(`v2/gallery/${code}/code`, options);
+    } catch(err) {
+        throw new utils.ConnectorError(err?.code, err?.message, err?.messageCode);
+    }
+};
+
+/**
  * Get items of a gallery
  * @async
  * @param {Object} params - URL params
@@ -19,5 +36,6 @@ const getItemsGallery = async ({code}, options = {}) => {
 };
 
 module.exports = {
+    getGallery,
     getItemsGallery
 }
